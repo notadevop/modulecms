78f3a72988dab734fe59b44b4a94528571742d4b
notadeveloper <10272078+notadevop@users.noreply.github.com>
2022-01-15 23:20:54 +0200

    upd#

--- Bin/Mailer.bin.php
+++ Bin/Mailer.bin.php
@@ -12,6 +12,53 @@ class Mailer {
 
 
     private $to; 	// кому
-    private $form; 	// от кого
+    private $from; 	// от кого
+    private $body;  // тело обьекта
+    private $subject; // заголовок
+
+
+    function initTo(string $to):void {
+
+        $this->to = $to;
+    }
+
+    function initFrom(string $from): void {
+
+        $this->from = $from;
+    }
+
+    function initSubject(string $subject):void {
+
+        $this->subject = $subject;
+    }
+
+    function initBody(string $body): void {
+
+        $this->body = $body;
+    }
+
+    function initMailTemplate(string $tplName): bool {
+
+        // тут достаем шаблон для емайла
+    }
+
+
+    function sendMessage(): bool {
+
+        $to     = $this->to;
+        $from   = $this->from;
+        $subject= $this->subject;
+        $body   = $this->body;
+
+
+        try {
+            if (empty($to) || empty($from) || empty($subject) || empty($body)) {
+                throw new Exception('у вас есть пустые поля!');
+            }
+            
+        } catch (Exception $e) {
+            return false;
+        }
+    }
 
 }
