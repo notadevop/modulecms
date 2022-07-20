2c8930ca351bd18afddd2a61f5e1b8ab464a5658
notadeveloper <10272078+notadevop@users.noreply.github.com>
2022-07-20 16:11:54 +0300

    Update PostAgregator.class.php

--- PostAgregator.class.php
+++ PostAgregator.class.php
@@ -10,17 +10,26 @@ class PostAgregator {
 
 	private static $savedData = array();
 
-	public static function initilizePostData():void {
+	// Сохраняем все переменные _POST 
 
-		self::$savedData = $_POST;
+	public static function initInputData():void {
+
+		self::$savedData = $_POST; // [0]
 
 		unset($_POST);
 	}
 
-	public static function getPostData($key, ) {
+	// по ключам массива вытаскиваем нужные переменные 
 
+	public static function getPostData(array $keys): ?array{
 
-
+		return !empty($keys) ? array_intersect(self::$savedData, $keys) : array();
 	}
 
+	function delegator($clouser) {
+
+		// тут даем исполнение для замыкания внешнего кода который можно было бы использовать 
+		// с сохраненными данными из _POST данных
+
+	}
 }
